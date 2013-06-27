fcb4ded7e446cef9e8dfddbd666b294a0ce3a460
halogenandtoast <halogenandtoast@gmail.com>
2013-06-27 14:08:00 -0400

    Initial app.

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'monban'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :test, :development do
+  gem 'rspec-rails', '2.14.0.rc1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tcg::Application.config.secret_key_base = '6f3ff5cd1ad3faefb894361b0ef16fa311db08bf7d04ddf4cae0a8ef7ef2deaa580183bfe9b817fb7bf49733d3e57595bad52d4071c284314e33129c11c137bf'
