662aacc867e68960095de43f6cdfc989ee11cce9
halogenandtoast <halogenandtoast@gmail.com>
2013-07-04 01:10:05 -0400

    Remove default scope.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def show
-    @collecting_sets = current_user.collecting_sets
+    @collecting_sets = current_user.collecting_sets.alphabetical
     @uncollecting_sets = CardSet.where.not(id: @collecting_sets.map(&:card_set_id))
   end
 end
--- app/models/collecting_set.rb
+++ app/models/collecting_set.rb
@@ -3,11 +3,14 @@ class CollectingSet < ActiveRecord::Base
   belongs_to :card_set
   has_many :collected_cards, dependent: :destroy
   has_many :card_versions, through: :card_set
-  default_scope { includes(:card_set).order("card_sets.name asc") }
 
   delegate :name, to: :card_set
 
   def full_count
     card_set.cards.count
+  end
+
+  def self.alphabetical
+    includes(:card_set).order("card_sets.name ASC")
   end
 end
